62577ac9081fc4c744592216bb8898ca42968376
Samantha Flores <740376@student.dusd.net>
2024-05-08 15:26:44 -0700

    Lesson 5.3
    Lesson 5.3 step 7

--- Unit5UserInterfaceFlores/Assets/Scripts/Target.cs
+++ Unit5UserInterfaceFlores/Assets/Scripts/Target.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class Target : MonoBehaviour
@@ -35,14 +36,21 @@ public class Target : MonoBehaviour
 
     private void OnMouseDown()
     {
-      Destroy(gameObject);
-      Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
-      gameManager.UpdateScore(pointValue);
+        if(gameManager.isGameActive)
+        {
+            Destroy(gameObject);
+            Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+            gameManager.UpdateScore(pointValue);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
     {
         Destroy(gameObject);
+        if(!gameObject.CompareTag("Bad"))
+        {
+            gameManager.GameOver();
+        }
     }
 
     Vector3 RandomForce()
